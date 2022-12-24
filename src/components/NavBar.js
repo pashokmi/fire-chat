c6d38ae298f8ea47d8d49e243d4e57629f767077
pashokmi <pashokmi@gmail.com>
2022-12-24 12:03:10 +0200

    test commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -46,7 +46,7 @@ const NavBar = () => {
                 ?
                 'Fire-CHAT'
                 :
-                'Fire-LOGIN'
+                'LOGIN'
               }
             </Text>
           </Flex>
